bf6cd0f4f6eed61e9934e655982e166659ef2f34
Cara Bean <carabean@Odysseus.local>
2017-12-21 15:40:52 -0500

    supply index page

--- app/controllers/api/v1/supplies_controller.rb
+++ app/controllers/api/v1/supplies_controller.rb
@@ -1,5 +1,8 @@
 # if statement will need to be added to def create to ensure sfs is associated with the correct supply_id
 class Api::V1::SuppliesController < ApiController
+  def index
+    render json: Supply.all
+  end
 
   def show
     @supply = Supply.find(params[:id])
