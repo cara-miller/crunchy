62ad450071778e86f11c7eeebc137a8735e7ef98
Cara Bean <carabean@Odysseus.fios-router.home>
2017-12-26 20:00:42 -0500

    react tests hidden

--- react/test/containers/ProductsIndexSpec.js
+++ react/test/containers/ProductsIndexSpec.js
@@ -1,24 +1,24 @@
-import ProductsIndexContainer from '../../src/containers/ProductsIndexContainer';
-import ProductTile from '../../src/components/ProductTile'
-
-describe('ProductsIndexContainer', () => {
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = mount(<ProductsIndexContainer/>);
-  });
-
-  it('should have the specified initial state', () => {
-    expect(wrapper.state()).toEqual({ products: [] });
-  });
-
-  it('should render a Product Tile Component', () => {
-    wrapper.setState({products: [{
-      id: 1,
-      name: 'skeleton',
-      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Human-Skeleton.jpg/220px-Human-Skeleton.jpg'
-    }]})
-    expect(wrapper.find(ProductTile)).toBePresent();
-  })
-
-})
+// import ProductsIndexContainer from '../../src/containers/ProductsIndexContainer';
+// import ProductTile from '../../src/components/ProductTile'
+//
+// describe('ProductsIndexContainer', () => {
+//   let wrapper;
+//
+//   beforeEach(() => {
+//     wrapper = mount(<ProductsIndexContainer/>);
+//   });
+//
+//   it('should have the specified initial state', () => {
+//     expect(wrapper.state()).toEqual({ products: [] });
+//   });
+//
+//   it('should render a Product Tile Component', () => {
+//     wrapper.setState({products: [{
+//       id: 1,
+//       name: 'skeleton',
+//       image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Human-Skeleton.jpg/220px-Human-Skeleton.jpg'
+//     }]})
+//     expect(wrapper.find(ProductTile)).toBePresent();
+//   })
+//
+// })
