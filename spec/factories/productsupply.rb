440bdf21cafcd3ddaa6a300e23e04e7f446e7d62
Cara Bean <carabean@Odysseus.local>
2018-01-16 17:31:19 -0500

    testing added

--- spec/factories/productsupply.rb
+++ spec/factories/productsupply.rb
@@ -1,12 +1,8 @@
-# FactoryBot.define do
-#   p1 = FactoryBot.create(:product)
-#   s1 =FactoryBot.create(:supply)
-#   factory :productsupply do
-#     supply s1
-#     product p1
-#     supply_id s1.id
-#     product_id p1.id
-#     quantity 2
-#     productsupplycost 15
-#   end
-# end
+FactoryBot.define do
+  factory :productsupply do
+    supply
+    product
+    quantity 2
+    user
+  end
+end
